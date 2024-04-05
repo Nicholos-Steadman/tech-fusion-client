00c365303f79dddb792a67bc01c9162dc0cc957b
Nicholos Steadman <steadman305@gmail.com>
2024-04-05 01:49:56 -0400

    variables/main component

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,17 @@
+import "./Main.scss"
+
+function Main() {
+    return(
+        <main className="main">
+        <div>
+            <h1 className="main__heading">
+                Begin customizing your experience!
+            </h1>
+            <p className="main__text">Complete your preference quiz now to tailor the app to your needs.</p>
+            <button className="main__button">Start Now</button>
+        </div>Í
+        </main>
+    )
+}
+
+export default Main;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,15 +5,7 @@ function Home() {
     return (
         <>
             <Header />
-            <main className="main">
-                <div>
-                    <h1 className="main__heading">
-                        Begin customizing your experience!
-                    </h1>
-                    <p className="main__text">Complete your preference quiz now to tailor the app to your needs.</p>
-                    <button className="main__button">Start Now</button>
-                </div>
-            </main>
+           <Main />
 
         </>
     )
